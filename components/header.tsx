8f2171481fd09e6fdf6dcf424a079c8079e3c256
ltran <loris.tran@epfl.ch>
2023-10-19 16:32:46 +0200

    initial commit

--- components/header.tsx
+++ components/header.tsx
@@ -1,63 +1,68 @@
-import DesktopOnly from '../responsiveness/DesktopOnly';
-import logo from "../../images/logo/logo_header.svg";
-import logotext from "../images/logo/logo_text_header.svg";
-import MobileOnly from '../responsiveness/MobileOnly';
-import Image from "next/image";
-import { useState } from 'react'
-import dynamic from "next/dynamic"
-//import Wallet from "./WalletConnect";
-import { HeaderButton } from '../ComponentBank/ComponentBank';
-import { ConnectButton } from '@rainbow-me/rainbowkit';
-import { Box, AppBar, Toolbar, IconButton, Typography, Button, Grid } from '@mui/material';
+import * as React from 'react';
+import logo from '../images/dai-logo.png';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
-import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import Switch from '@mui/material/Switch';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormGroup from '@mui/material/FormGroup';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
+import Image from "next/image";
+import { ConnectButton } from '@rainbow-me/rainbowkit';
 
-export default function Nav() {
-    return (
-      <Box flexGrow={1} paddingBottom={14}>
-        <AppBar position="fixed">
-          <Toolbar>
-            <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              sx={{mr: 2}}
-              
-            >
-              <Image src={logo} alt="" />
-            </IconButton>
-            <Grid container  spacing={0}>
-                <Grid item xs={4} container flexGrow={1} spacing={4}>
-                    
-                    <Grid item  display="flex" justifyContent={'flex-start'} alignItems={'center'} >
-                        <Typography variant="h5" component="div" >
-                            <HeaderButton target="" text="Pools" href="/Pools"/>
-                        </Typography>
-                    </Grid>
-                    <Grid item  display="flex" justifyContent={'flex-start'} alignItems={'center'} >
-                        <Typography variant="h5" component="div" >
-                            <HeaderButton target="" text="Vaults" href="/Vaults"/>
-                        </Typography>
-                    </Grid>
-                    <Grid item display="flex" justifyContent={'flex-start'} alignItems={'center'} >
 
-                        <Typography variant="h5" component="div" >
-                            <HeaderButton target="" text="Explorer" href="/explore"/>
-                        </Typography>
-                    </Grid>
-                    <Grid item  display="flex" justifyContent={'flex-start'} alignItems={'center'} >
-                        <IconButton>
-                            <MoreHorizIcon />
-                        </IconButton>
-                    </Grid>
-                </Grid>
-                
-                    <Grid item xs={8}  display="flex"justifyContent={'flex-end'}><ConnectButton chainStatus={{smallScreen: "icon", largeScreen: "icon"}} showBalance={{ smallScreen: false, largeScreen: false }}/></Grid>
-            </Grid> 
-            
-          </Toolbar>
-        </AppBar>
-      </Box>
-    );
-  }
+export default function MenuAppBar() {
+  const [auth, setAuth] = React.useState(true);
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setAuth(event.target.checked);
+  };
+
+  const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <Box sx={{ flexGrow: 1 , color : '#151515'}}>
+      
+      <AppBar position="static">
+        <Toolbar sx={{backgroundColor : '#151515'}}>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+              <Image src={logo} 
+              alt="" 
+              width={50}
+              height={50}/>
+          </IconButton>
+          <Typography variant="h6" component="div" paddingX={3}>
+            Vault
+          </Typography>
+          <Typography variant="h6" component="div" paddingX={3}>
+            About
+          </Typography>
+          <Typography variant="h6" component="div" paddingX={3}>
+            Vote
+          </Typography>
+          <Box sx={{  display: 'flex' , justifyContent: 'flex-end' , alignContent : 'flex-end'}}>
+            <ConnectButton />
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import '../styles/globals.css';
 import '@rainbow-me/rainbowkit/styles.css';
-import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { getDefaultWallets, RainbowKitProvider, darkTheme } from '@rainbow-me/rainbowkit';
 import type { AppProps } from 'next/app';
 import { configureChains, createConfig, WagmiConfig } from 'wagmi';
 import {
@@ -11,6 +11,8 @@ import {
   polygon,
 } from 'wagmi/chains';
 import { publicProvider } from 'wagmi/providers/public';
+import { ThemeProvider, createTheme } from '@mui/material';
+
 
 const { chains, publicClient, webSocketPublicClient } = configureChains(
   [
@@ -36,11 +38,44 @@ const wagmiConfig = createConfig({
   webSocketPublicClient,
 });
 
+
+
+export const theme = createTheme({
+  
+palette: {
+  primary: {
+    // light: will be calculated from palette.primary.main,
+    light : '#FFFFFF',
+    main: '#E8ECFB',
+    // dark: will be calculated from palette.primary.main,
+    // contrastText: will be calculated to contrast with palette.primary.main
+  },
+  secondary: {
+    light: '#FB118E', //uniswap purple light mode
+    main: '#FB118E', //uniswap purple main mode
+    // dark: will be calculated from palette.secondary.main,
+    contrastText: '#FDEAF1',
+  },
+  background: {
+    paper: '#FFFFFF',
+  },
+  text: {
+    primary: "#000000",
+    secondary : "7780A0",
+   
+  }
+  
+},
+
+});
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <WagmiConfig config={wagmiConfig}>
-      <RainbowKitProvider chains={chains}>
-        <Component {...pageProps} />
+      <RainbowKitProvider chains={chains} theme={darkTheme()}>
+        <ThemeProvider theme={theme}>
+          <Component {...pageProps} />
+        </ThemeProvider>
       </RainbowKitProvider>
     </WagmiConfig>
   );
